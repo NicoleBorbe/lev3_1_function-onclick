d57413025c6b3fbd0af11b8f94546103048fa8b8
NicoleBorbe <nicole.borbe@gmail.com>
2022-08-18 20:45:19 +0200

    vision1

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+function change() {
+    document.body.style.backgroundColor = "green";
+    document.body.style.color = "purple";
+}
+function change2() {
+    document.body.style.backgroundColor = "rgb(14, 56, 85)";
+    document.body.style.color = "rgb(247, 247, 25)";
+}
+function change3() {
+    document.body.style.backgroundColor = "black";
+    document.body.style.color = "orange";
+}
+function change4() {
+    document.body.style.backgroundColor = "navy";
+    document.body.style.color = "lightblue";
+}
